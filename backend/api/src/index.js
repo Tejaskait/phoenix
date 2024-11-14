b40f24405361a750ba6c6347d32e7db010a40f46
Tejas <tejaskait@gmail.com>
2024-11-15 00:25:54 +0530

    "forontend and backend setup "

---
+++ backend/api/src/index.js
@@ -0,0 +1,9 @@
+// Imports
+const express = require('express')
+
+const app = express()
+const port = 8000
+
+app.get('/', (req, res) => res.send('phoenix/ backend/api'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
