c0f885525eb446809056b60aad4878b3ecc89f9c
Tejas <tejaskait@gmail.com>
2024-11-16 00:03:46 +0530

    "Added react-router-dom dependency and implemented routing in App component"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import {Routes,Route} from 'react-router-dom'
+import Home from "./pages/home";
+import createemployee from "./pages/createemployee";
+import showemployee from "./pages/showemployee";
+import editemployee from "./pages/editemployee";
+import deleteemployee from "./pages/deleteemployee";
 
 const App = () => {
  return (
-   <div className='bg-red-400 text-white'><h1>Welcome to Phoenix</h1></div>
+   <Routes>
+     <Route path="/" element={<home />} />
+     <Route path="/employee/create" element={<createemployee />} />
+     <Route path="/employee/details/:id" element={<showemployee />} />
+     <Route path="/employee/edit/:id" element={<editemployee />} />
+     <Route path="/employee/delete/:id" element={<deleteemployee />} />
+   </Routes>
  )
 }
 
